625609296123472f01192045cb2a31f8a4d0e115
uloydev <uloydev@gmail.com>
2022-01-16 13:44:30 +0700

    fix proposal result page

--- resources/views/proposal/result.blade.php
+++ resources/views/proposal/result.blade.php
@@ -8,15 +8,21 @@
         <!-- Register Card-->
         <div
             class=" bg-white max-w-2xl w-full space-y-8 px-4 sm:px-6 py-7 rounded-3xl text-black flex flex-col items-center tracking-tighter">
-            <img src="{{asset('/img/brin-logo-black.png')}}" alt="brin logo" class="w-48 sm:w-60">
+            <img src="{{ asset('/img/brin-logo-black.png') }}" alt="brin logo" class="w-48 sm:w-60">
             <div class="w-full">
                 <p class="mt-2">Nama Lengkap : {{ $proposal->full_name }}</p>
                 <p class="mt-2">Nama Instansi &nbsp; : {{ $proposal->instance }}</p>
                 <p class="mt-2">Judul Penelitian : {{ $proposal->title }}</p>
                 <p class="mt-2">Jenis Bidang &nbsp; &nbsp; &nbsp; : {{ $proposal->proposalCategory->name }}</p>
-                <p class="text-2xl text-green-500 font-bold text-center my-12">SELEMAT PENGAJUAN ANDA DI SETUJUI</p>
-                <p class="text-2xl text-red-500 font-bold text-center my-12">MAAF PENGAJUAN ANDA TIDAK DI SETUJUI</p>
-                <p>tahap selanjutnya untuk mengklaim pendanaan riset, anda diharapkan untuk memenuhi persyaratan dibawah ini :</p>
+                @if (!$proposal->score)
+                    <p class="text-2xl text-yellow-500 font-bold text-center my-12">PENGAJUAN ANDA BELUM DINILAI</p>
+                @elseif ($proposal->score > 70)
+                    <p class="text-2xl text-green-500 font-bold text-center my-12">SELEMAT PENGAJUAN ANDA DI SETUJUI</p>
+                @else
+                    <p class="text-2xl text-red-500 font-bold text-center my-12">MAAF PENGAJUAN ANDA TIDAK DI SETUJUI</p>
+                @endif
+                <p>tahap selanjutnya untuk mengklaim pendanaan riset, anda diharapkan untuk memenuhi persyaratan dibawah ini
+                    :</p>
                 <br>
                 <p>Dokumen kontrak riset</p>
                 <a href="https://s.id/kelengkapanpendanaanriset2021">https://s.id/kelengkapanpendanaanriset2021</a>
